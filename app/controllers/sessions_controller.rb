77989dbf0881fa16a7890a4dd290a7c11657469d
Tomohiro Shimpo <tomohiro.shimpo@gmail.com>
2022-03-19 16:46:53 +0000

    logins success : redirect to user page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      #ユーザログイン後にユーザー情報ページにリダイレクト
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
