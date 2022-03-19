f63f49f5b872822e0f7d01f025b22cd75abdf5bd
Tomohiro Shimpo <tomohiro.shimpo@gmail.com>
2022-03-19 17:46:31 +0000

    add method : user is logged in now or not

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,4 +4,17 @@ module SessionsHelper
   def log_in(user)
     session[:user] = user.id
   end
+  
+  #現在ログイン中のユーザを返す(いる場合)
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
+  end
+  
+  #ユーザーがログインしていればtrue,その他はfalse
+  def logged_in?
+    !current_user.nil?
+  end
+  
 end
