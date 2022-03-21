9a5bf1cab0165061c9cd4117b15d34ef22f79550
Tomohiro Shimpo <tomohiro.shimpo@gmail.com>
2022-03-21 15:04:28 +0000

    create actoin with friendly forwarding

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -47,4 +47,14 @@ module SessionsHelper
     session.delete(:user_id)
     @current_user = nil
   end
+  
+  # 記憶したURLにリダイレクト
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end 
+  
+  def store_location
+     session[:forwarding_url] = request.original_url if request.get?
+  end
 end
