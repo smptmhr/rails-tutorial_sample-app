abe3b28ee00dad7293cdf59d78d13e9617cd69a7
Tomohiro Shimpo <tomohiro.shimpo@gmail.com>
2022-03-22 09:44:35 +0000

    user activation is default false

--- db/migrate/20220322091822_add_activation_to_users.rb
+++ db/migrate/20220322091822_add_activation_to_users.rb
@@ -1,7 +1,7 @@
 class AddActivationToUsers < ActiveRecord::Migration[5.1]
   def change
     add_column :users, :avtivation_digest, :string
-    add_column :users, :activated, :boolean
+    add_column :users, :activated, :boolean, default: falsed
     add_column :users, :activated_at, :datetime
   end
 end
