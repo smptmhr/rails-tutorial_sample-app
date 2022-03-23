099fee2796e4d7ceae295ac83ec07ec2f8fc7468
Tomohiro Shimpo <tomohiro.shimpo@gmail.com>
2022-03-23 06:30:23 +0000

    fix bug : activate action is valid for user instance

--- app/models/user.rb
+++ app/models/user.rb
@@ -49,8 +49,8 @@ class User < ApplicationRecord
 
   # アカウントを有効にする
   def activate
-    update_attribute(:activated,    true)
-    update_attribute(:activated_at, Time.zone.now)
+    user.update_attribute(:activated,    true)
+    user.update_attribute(:activated_at, Time.zone.now)
   end
   
   # 有効化用のメールを送信する
