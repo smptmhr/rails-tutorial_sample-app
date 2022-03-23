f687a66331222c9f0f85148682c247df0d3c11ab
Tomohiro Shimpo <tomohiro.shimpo@gmail.com>
2022-03-23 05:26:40 +0000

    fix host url

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :test
-  host = 'ip-172-31-12-180' 
+  host = '9318e2402b5a46ada026175e522d2936.vfs.cloud9.us-east-2.amazonaws.com' 
   config.action_mailer.default_url_options = { host: host, protocol: 'https' }
 
   # Print deprecation notices to the Rails logger.
