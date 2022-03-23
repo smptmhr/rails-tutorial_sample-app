cf2ec5c5e42db58989ebc80eeaff030ae1ad7179
Tomohiro Shimpo <tomohiro.shimpo@gmail.com>
2022-03-23 16:24:42 +0000

    add feed to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -76,6 +76,11 @@ class User < ApplicationRecord
     reset_sent_at < 2.hours.ago
   end
   
+  # 試作feedの定義
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
+  
   private
     
     # メールアドレスを全て小文字にする
